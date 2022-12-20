08b67c2a44ff5791026299d76fd84e3edb19d1e7
PinoMacri <pinonegozio1@outlook.it>
2022-12-20 21:42:55 +0100

    Milestone 5, quasi finito

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 //! ------------------------------ELEMENTI DAL DOM------------------------------------
 const realGrid = document.getElementById("realGrid")
 const button = document.getElementById("button")
+const punti = document.getElementById("scored")
+const allButton = document.querySelectorAll("button")
+const popup = document.getElementById ("popup")
+const risultato = document.getElementById ("risultato")
 //! ----------------------------------FUNZIONI----------------------------------------
-//^ FUNZIONE 1 : "Formula Creazione Singola Cella"
+//^ FUNZIONE 1 : "Formula Creazione Singola Cella+Appensione"
 function CellaFinita() {
     //* Creo e Inserisco la cella
     const div = document.createElement("button")
@@ -17,7 +21,7 @@ const unicoNumeroRandom = (min = 1, max = 100, blackList) => {
     let randomNumber;
     do {
         randomNumber = Math.floor(Math.random() * (max + 1 - min)) + min;
-    } while (blackList.includes (randomNumber))
+    } while (blackList.includes(randomNumber))
     return randomNumber;
 }
 //! ----------------------------LOGICA DI SVOLGIMENTO---------------------------------
@@ -28,7 +32,7 @@ button.addEventListener("click", function () {
 })
 //^ PASSAGGIO 2 : Apro un Ciclo per completare la Griglia e al Click della cella devo aumentare il punteggio : 
 //* Preparo il mio contatore
-let counter = 0
+let counter = [0]
 //* Apro il ciclo
 for (let i = 1; i <= 100; i++) {
     //* Genero 100 celle
@@ -37,31 +41,32 @@ for (let i = 1; i <= 100; i++) {
     cellaUtilizzoUno.append(i)
     //* Coloro le Celle al Click & Le rendo inutilizzabili al secondo Click : 
     cellaUtilizzoUno.addEventListener("click", function () {
-        console.log(i)
-        if (cellaUtilizzoUno.classList.contains("azzurro")) {
-            cellaUtilizzoUno.classList.remove("azzurro")
-            cellaUtilizzoUno.disabled = true
-        } else {
+        cellaUtilizzoUno.disabled = true
+        ++counter
+        //*Verifico se l'utente calpesta una bomba
+        let hasHitBomb;
+        if (hasHitBomb = extractedNumber.includes(i)) {
+            cellaUtilizzoUno.classList.add("red")
+            risultato.innerText = `La partita è finita! il tuo punteggio è stato di ${(counter) - 1} punti!`
+            popup.classList.add("activeDue")
+        } else if (!(hasHitBomb = extractedNumber.includes(i))){
             cellaUtilizzoUno.classList.add("azzurro")
-            cellaUtilizzoUno.disabled = true
-        }
-        //* Stampo in console il punteggio
-        console.log(++counter)
+        } 
+        console.log(counter)
     })
 }
 //^ PASSAGGIO 3 : Renderizzo le celle e genero 16 numeri random MAI uguali : 
 //* Preparo l'Array che ospiterà i numeri usciti
 const extractedNumber = [];
-for (let i=1; i <= 16 ; i++ ) {
+for (let i = 1; i <= 16; i++) {
     //* Genero un numero random
-    const cellNumber = unicoNumeroRandom (1, 100, extractedNumber)
+    const cellNumber = unicoNumeroRandom(1, 100, extractedNumber)
     //* Aggiungo il numero uscito ai numeri usciti
     extractedNumber.push(cellNumber)
 }
 //* Vedo la mia blacklist di numeri estratti
 console.log(extractedNumber)
-
-
-
-
-
+//*--------------------------------------------*/
+function rigioca () {
+    location.reload()
+}
